6b365cedce8c7442f3d0eb143c0eb4dd19533b0e
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-23 02:44:15 +0800

    make it pretty

--- backup.sh
+++ backup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws cloudwatch get-dashboard --region us-west-2 --dashboard-name friends-capstone-temporary-RDS | jq ".DashboardBody"
+aws cloudwatch get-dashboard --region us-west-2 --dashboard-name friends-capstone-temporary-RDS | jq ".DashboardBody" | python -m json.tool

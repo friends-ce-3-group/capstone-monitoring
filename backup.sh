983ea53a33cb76ffadbd896150c4f76140012066
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-23 10:09:09 +0800

    add brackets

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-body=aws cloudwatch get-dashboard --region us-west-2 --dashboard-name friends-capstone-temporary-RDS | jq ".DashboardBody"
+body=$(aws cloudwatch get-dashboard --region us-west-2 --dashboard-name friends-capstone-temporary-RDS | jq ".DashboardBody")
 echo $body
 echo "----------------------------"
 echo $body | python -m json.tool

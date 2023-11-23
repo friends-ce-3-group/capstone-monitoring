f41f24ba58770e0fd2359b653c266c370fd32280
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-23 11:06:56 +0800

    show created files

--- backup.sh
+++ backup.sh
@@ -11,6 +11,7 @@ do
     dashfile="$dash.json"
     aws cloudwatch get-dashboard --region us-west-2 --dashboard-name $dash | jq ".DashboardBody" > $dashfile
     python3 backup.py --json_file $dashfile
-    echo "\n$dash:\n"
+    echo ""
+    echo "$dash:"
     cat $dashfile
 done

1d86e0a23b1b79a3331739aba1c56dc8c78e042b
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-23 11:08:56 +0800

    parameterize region

--- backup.sh
+++ backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+region=us-west-2
 dashboards=(
     friends-capstone-temporary-RDS 
     friends-capstone-temporary-ManagementEvents
@@ -9,7 +10,7 @@ dashboards=(
 for dash in "${dashboards[@]}"
 do
     dashfile="$dash.json"
-    aws cloudwatch get-dashboard --region us-west-2 --dashboard-name $dash | jq ".DashboardBody" > $dashfile
+    aws cloudwatch get-dashboard --region $region --dashboard-name $dash | jq ".DashboardBody" > $dashfile
     python3 backup.py --json_file $dashfile
     echo ""
     echo "$dash:"
